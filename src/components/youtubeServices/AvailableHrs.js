069eb223b429ad6f1f4242a05f7da4013744fdfd
vivek <vivekknit786@gmail.com>
2024-09-20 15:57:54 +0530

    updated logic for timer

---
+++ src/components/youtubeServices/AvailableHrs.js
@@ -0,0 +1,84 @@
+import { Col, Container, Image, ListGroup, Row } from "react-bootstrap";
+
+import { useEffect, useState } from "react";
+import AppImages from "../../utils/images";
+
+const createTimerArray = () => {
+  var hours, minutes, ampm;
+  var time = [];
+  // 495 means starting from 8:15 AM to 11:30 PM
+  for (var i = 480; i <= 1420; i += 15) {
+    hours = Math.floor(i / 60);
+    minutes = i % 60;
+    if (minutes < 10) {
+      minutes = "0" + minutes; // adding leading zero
+    }
+    ampm = hours % 24 < 12 ? "AM" : "PM";
+    hours = hours % 12;
+    if (hours === 0) {
+      hours = 12;
+    }
+    time.push(hours + ":" + minutes + " " + ampm);
+  }
+  return time;
+};
+const prepareAvailableHrs = () => {
+  const items = createTimerArray();
+  const objArray = [];
+  for (let i = 0; i < items.length - 1; i++) {
+    objArray.push({
+      start: items[i],
+      end: items[i + 1],
+      isActive: false,
+    });
+  }
+  return objArray;
+};
+
+const AvailableHrs = () => {
+  const availableHrs = prepareAvailableHrs();
+  const [hrsArray, setHrsArray] = useState([]);
+
+  useEffect(() => {
+    setHrsArray(availableHrs);
+  }, []);
+
+  const selectedItem = (id) => {
+    const currentHrs = [...hrsArray];
+    const updateHrs = currentHrs.map((h) => {
+      return {
+        ...h,
+        isActive: false,
+      };
+    });
+
+    const selected = updateHrs[id];
+    selected.isActive = true;
+    setHrsArray([...updateHrs]);
+  };
+  return (
+    <ListGroup className="timer-row" horizontal>
+      {hrsArray.map((item, idx) => {
+        return (
+          <Col
+            className={` timer-section ${item.isActive ? "active-timer" : ""}`}
+            key={`timer-section-${idx}`}
+            onClick={() => {
+              selectedItem(idx);
+            }}
+          >
+            <Row>
+              <Image src={AppImages.timer} />
+              <Container>
+                <span>{item.start}</span>
+                <p>{item.end}</p>
+              </Container>
+            </Row>
+          </Col>
+        );
+      })}
+    </ListGroup>
+  );
+};
+
+export default AvailableHrs;
--- src/components/youtubeServices/scheduleMeeting.js
+++ src/components/youtubeServices/scheduleMeeting.js
@@ -1,92 +1,19 @@
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Col, Container, Image, ListGroup, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Link, useLocation } from "react-router-dom";
-import { ArrowLeftCircle } from "react-bootstrap-icons";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import AppImages from "../../utils/images";
-const availableHrs = [
-  {
-    start: "08:00 AM",
-    end: "09:00 AM",
-  },
-  {
-    start: "09:00 AM",
-    end: "10:00 AM",
-  },
-  {
-    start: "10:00 AM",
-    end: "11:00 AM",
-  },
-  {
-    start: "11:00 AM",
-    end: "12:00 AM",
-  },
-  {
-    start: "12:00 PM",
-    end: "01:00 PM",
-  },
-  {
-    start: "01:00 PM",
-    end: "02:00 PM",
-  },
-  {
-    start: "02:00 PM",
-    end: "03:00 PM",
-  },
-  {
-    start: "03:00 PM",
-    end: "04:00 PM",
-  },
-  {
-    start: "04:00 PM",
-    end: "05:00 PM",
-  },
-  {
-    start: "05:00 PM",
-    end: "06:00 PM",
-  },
-  {
-    start: "06:00 PM",
-    end: "07:00 PM",
-  },
-  {
-    start: "07:00 PM",
-    end: "08:00 PM",
-  },
-  {
-    start: "08:00 PM",
-    end: "09:00 PM",
-  },
-];
-const AvailableHrs = () => {
-  return (
-    <Row className="timer-row">
-      {availableHrs.map((item, idx) => {
-        return (
-          <Col sm={2} className="timer-section" key={`timer-section-${idx}`}>
-            <Row>
-              <Image src={AppImages.timer} />
-              <span>{item.start}</span>
-              <p>{item.end}</p>
-            </Row>
-          </Col>
-        );
-      })}
-    </Row>
-  );
-};
+import AvailableHrs from "./AvailableHrs";
 
 function ScheduleMeeting() {
   const { t } = useTranslation();
   const { state } = useLocation();
   const [startDate, setStartDate] = useState(new Date());
   console.log("Added Services", state, startDate);
-  let handleColor = (time) => {
-    return time.getHours() > 12 ? "text-success" : "text-error";
-  };
+
   return (
     <section id="cart-services" className="cart-services">
       <Row>
@@ -106,8 +33,8 @@ function ScheduleMeeting() {
               <Col>
                 {/* https://reactdatepicker.com/ */}
                 <DatePicker
-                  showTimeSelect
-                  timeIntervals={15}
+                  //  showTimeSelect
+                  // timeIntervals={15}
                   //timeClassName={handleColor}
                   // formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 3)}
                   minDate={new Date()}
